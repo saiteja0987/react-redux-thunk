c8610974bc348ffe0e5fe5cfc90df03e1eb9c273
Sai Teja <saitejapubglite@gmail.com>
2024-03-03 17:41:17 +0530

    done

---
+++ react-redux-thunk/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Counter from "./assets/Counter";
+
+export default class App extends React.Component{
+
+  render(){
+    return (
+      <div style={{textAlign:"center"}}>
+        <Counter/>
+      </div>
+    )
+  }
+  
+}
